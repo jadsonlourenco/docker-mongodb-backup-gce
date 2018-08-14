1f06bfd09e8bb6ff1bf4b327db67388aa341779c
ovvn <omazinov@gmail.com>
2018-08-14 14:20:59 +0300

    Add cleaning docs script. [backup, clean] Send notifications.

--- mongodb-backup.sh
+++ mongodb-backup.sh
@@ -6,6 +6,7 @@ DB_NAME="$MONGO_DATABASE"
 DB_USER="$MONGO_USER"
 DB_PASS="$MONGO_PASS"
 BUCKET_NAME="$BUCKET"
+PD_TOKEN="$PAGERDUTY_TOKEN"
 
 # Path in which to create the backup (will get cleaned later)
 BACKUP_PATH="/mnt/data/dump/"
@@ -16,7 +17,7 @@ CURRENT_DATE=$(date +"%Y%m%d-%H%M")
 BACKUP_FILENAME="$DB_NAME-$CURRENT_DATE.tar.gz"
 
 # Create the backup
-mongodump -h "$DB_HOST" -d "$DB_NAME" -u "$DB_USER" -p "$DB_PASS" -o "$BACKUP_PATH"
+mongodump -h "$DB_HOST" -d "$DB_NAME" -u "$DB_USER" -p "$DB_PASS" -o "$BACKUP_PATH" || curl -X POST --header ‘Content-Type: application/json' --header 'Accept: application/vnd.pagerduty+json;version=2' --header 'From: admin@idecisiongames.com' --header 'Authorization: Token token="$PD_TOKEN"' -d '{"incident":{"type":"incident","title":"IDG Backup failed.","service":{"id":"PONW4BQ","type":"service_reference"}}}' 'https://api.pagerduty.com/incidents'
 cd $BACKUP_PATH || exit
 
 # Archive and compress
---
+++ mongodb-cleandocs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Settings
+DB_HOST="$MONGO_HOST"
+DB_NAME="$MONGO_DATABASE"
+DB_USER="$MONGO_USER"
+DB_PASS="$MONGO_PASS"
+PD_TOKEN="$PAGERDUTY_TOKEN"
+
+# Connect to mongo
+mongo --quiet -u "$DB_USER" -p "$DB_PASS" -h "$DB_HOST" || curl -X POST --header ‘Content-Type: application/json' --header 'Accept: application/vnd.pagerduty+json;version=2' --header 'From: admin@idecisiongames.com' --header 'Authorization: Token token="$PD_TOKEN"' -d '{"incident":{"type":"incident","title":"[Clean docs] Couldn't connect to db.","service":{"id":"PONW4BQ","type":"service_reference"}}}' 'https://api.pagerduty.com/incidents'
+
+# Switch to idg
+use "$DB_NAME"
+
+# Clean ops
+db.getCollectionNames().forEach(function (c) {if(c.indexOf('o_') < 0) return; db[c].remove(({'m.ts': {'$lt': new Date().getTime() - 7 * 24 * 60 * 60 * 1000}}));}) || curl -X POST --header ‘Content-Type: application/json' --header 'Accept: application/vnd.pagerduty+json;version=2' --header 'From: admin@idecisiongames.com' --header 'Authorization: Token token="$PD_TOKEN"' -d '{"incident":{"type":"incident","title":"IDG ops cleaning failed.","service":{"id":"PONW4BQ","type":"service_reference"}}}' 'https://api.pagerduty.com/incidents'
+
+# Clean tasks
+db.tasks.remove(({'createdAt': {'$lt': new Date().getTime() - 24 * 60 * 60 * 1000}})) || curl -X POST --header ‘Content-Type: application/json' --header 'Accept: application/vnd.pagerduty+json;version=2' --header 'From: admin@idecisiongames.com' --header 'Authorization: Token token="$PD_TOKEN"' -d '{"incident":{"type":"incident","title":"IDG tasks cleaning failed.","service":{"id":"PONW4BQ","type":"service_reference"}}}' 'https://api.pagerduty.com/incidents'
+db.o_tasks.remove(({'m.ts': {'$lt': new Date().getTime() - 24 * 60 * 60 * 1000}}))
+
+exit
